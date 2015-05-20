96fbb34f0b92a6ee7a663029985d0d7fa0e9d80a
LudmilaBanaru <ludmilabanaru@gmail.com>
2015-05-20 18:42:44 +0200

    ParkFind renamed

--- app/src/main/java/ch/unige/idsi/y15/parkfind_android/TestActivity.java
+++ app/src/main/java/ch/unige/idsi/y15/parkfind_android/TestActivity.java
@@ -67,7 +67,6 @@ public class TestActivity extends ActionBarActivity
 
     public void restoreActionBar() {
         ActionBar actionBar = getSupportActionBar();
-       // actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
         actionBar.setDisplayShowTitleEnabled(true);
         actionBar.setTitle(mTitle);
     }
